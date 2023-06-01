68231c0d1eaecebf0ea207d52a552b1e179b60f2
Jesus Salinas <20030389@itcelaya.edu.mx>
2023-06-01 01:23:10 -0600

    Se agrego el boton de cerrar sesión

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 
@@ -8,15 +9,36 @@ function App() {
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
 
- 
+  const [token, setToken] = useState("")
+
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
+      setToken(token)
+    }
+
+  }, [])
+
+
+  const salir = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Spotify API con React</h1>
+        {!token ?
           <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Iniciar sesión</a>
-
-        
+          : <button onClick={salir}>Cerrar sesión</button>}
       </header>
     </div >
   );
